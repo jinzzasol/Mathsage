fa3f59f167558ceadecd401966254d3a3779c92f
ConnorDunlop <dunlopcw00@gmail.com>
2023-11-28 12:26:25 -0500

    updating previous_experiments

---
+++ Final/previous_experiments/configurations.py
@@ -0,0 +1,48 @@
+CONST_LIST_MATHQA = ["const_pi", "const_2", "const_1", "const_3", "const_4", "const_6", "const_10", "const_12", "const_100", "const_1000",
+                     "const_60", "const_3600", "const_1.6", "const_0.6", "const_0.2778", "const_0.3937", "const_2.54",
+                     "const_0.4535", "const_2.2046", "const_3.6", "const_deg_to_rad", "const_180", "const_0.25", "const_0.33",
+                     "#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16",
+                     "none"]
+
+CONST_LIST_MATHQA_2 = ["const_pi", "const_2", "const_1", "const_3", "const_4", "const_6", "const_10", "const_12", "const_100", "const_1000",
+                     "const_60", "const_3600", "const_1.6", "const_0.6", "const_0.2778", "const_0.3937", "const_2.54",
+                     "const_0.4535", "const_2.2046", "const_3.6", "const_deg_to_rad", "const_180", "const_0.25", "const_0.33",
+                     "#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16",
+                     "none"]
+
+OPERATION_LIST_MATHQA = ["add", "subtract", "multiply", "divide", "gcd", "lcm", "power", "max", "min",
+                         "reminder", "round", "radians_to_degress", "degree_to_radians",
+                         "sum_consecutive_number", "circle_arc", "semi_circle_perimiter", "circle_sector_area",
+                         "rectangle_perimeter", "rectangle_area", "trapezium_area",
+                         "rhombus_area", "quadrilateral_area", "volume_cone", "volume_rectangular_prism",
+                         "volume_cylinder", "surface_cone", "surface_cylinder", "surface_rectangular_prism",
+                         "side_by_diagonal", "diagonal", "triangle_perimeter",
+                         "triangle_area", "triangle_area_three_edges", "union_prob", "combination", "permutation", "count_interval",
+                         "percent", "p_after_gain", "p_after_loss", "price_after_gain", "price_after_loss", "from_percent", "gain_percent",
+                         "loss_percent", "negate_percent", "original_price_before_gain", "original_price_before_loss", "to_percent", "speed",
+                         "combined_work", "find_work", "speed_ratio_steel_to_stream", "speed_in_still_water", "stream_speed", 
+                         "floor", "cosine",
+                         "cube_edge_by_volume", "volume_cube", "sine", "factorial", "square_area", "negate", "sqrt", "circle_area",  "surface_sphere",
+                         "log", "surface_cube", "rhombus_perimeter", "volume_sphere", "tangent", "square_perimeter", "circumface", "square_edge_by_area",
+                         "inverse", "square_edge_by_perimeter", "negate_prob",
+                         "EOF"]
+
+OPERATION_LIST_MATHQA_2 = ["add", "subtract", "multiply", "divide", "gcd", "lcm", "power", "max", "min",
+                         "reminder", "round", "radians_to_degress", "degree_to_radians",
+                         "sum_consecutive_number", "circle_arc", "semi_circle_perimiter", "circle_sector_area",
+                         "rectangle_perimeter", "rectangle_area", "trapezium_area",
+                         "rhombus_area", "quadrilateral_area", "volume_cone", "volume_rectangular_prism",
+                         "volume_cylinder", "surface_cone", "surface_cylinder", "surface_rectangular_prism",
+                         "side_by_diagonal", "diagonal", "triangle_perimeter",
+                         "triangle_area", "triangle_area_three_edges", "union_prob", "combination", "permutation", "count_interval",
+                         "percent", "p_after_gain", "p_after_loss", "price_after_gain", "price_after_loss", "from_percent", "gain_percent",
+                         "loss_percent", "negate_percent", "original_price_before_gain", "original_price_before_loss", "to_percent", "speed",
+                         "combined_work", "find_work", "speed_ratio_steel_to_stream", "speed_in_still_water", "stream_speed", 
+                         "floor", "cosine",
+                         "cube_edge_by_volume", "volume_cube", "sine", "factorial", "square_area", "negate", "sqrt", "circle_area",  "surface_sphere",
+                         "log", "surface_cube", "rhombus_perimeter", "volume_sphere", "tangent", "square_perimeter", "circumface", "square_edge_by_area",
+                         "inverse", "square_edge_by_perimeter", "negate_prob",
+                         "GO", ")", "EOF"]
+
+OPERATION_LIST_DROP_ANNOTATED = ['biggest', 'subtract', 'add', 'multiply', 'divide', 'smallest', 'secondsmallest', 'EOF']
+CONST_LIST_DROP_ANNOTATED = ['const_0', 'const_1', 'const_100', 'const_1000000',  'const_1000000000', '#0', '#1', '#2', '#3', '#4', "#5", "#6", "none"]
